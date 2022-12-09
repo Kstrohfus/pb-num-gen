85b7a97ee8ed12655ea2e9051cd03dcf9a2867eb
Kstrohfus <houston11@me.com>
2022-12-09 11:13:22 -0700

    Still trying to figure it out...condensed lines in the meantime

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 
-let one = document.querySelector(".one")
-let pb = document.querySelector(".pb")
-const btn = document.querySelector(".btn")
+const one = document.querySelector(".one");
+const pb = document.querySelector(".pb");
+const btn = document.querySelector(".btn");
+
+    
 
 function randomNum() {
-    const num = Math.floor(Math.random() * 69) + 1;
+const num =Math.floor(Math.random() * 26) + 1;
 one.innerText = num
 }
 
 function randomNumPb() {
-const pbNum =Math.floor(Math.random() * 26) + 1;
-pb.innerText = pbNum
+const numpb =Math.floor(Math.random() * 26) + 1;
+pb.innerText = numpb
 }
 
 btn.addEventListener("click", function (){
@@ -18,4 +20,9 @@ randomNum()
 randomNumPb()
 })
 
-//let numElements = [".one", ".two", ".three", ".four", ".five", ".pb"]//
+
+// Different ways...Need to figure out to lump elements into function...
+
+// let numElements = document.querySelectorAll([".one", ".two", ".three", ".four", ".five", ".pb"])
+
+// let numElements = [...document.getElementsByClassName("num")];
