3d783be8c2d0653f3f82193f96d74ab30f5550a3
Kstrohfus <houston11@me.com>
2022-12-08 23:47:27 -0700

    Added click function

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
 let one = document.querySelector(".one")
 let pb = document.querySelector(".pb")
+const btn = document.querySelector(".btn")
 
 function randomNum() {
     const num = Math.floor(Math.random() * 69) + 1;
-
 one.innerText = num
 }
-randomNum()
 
 function randomNumPb() {
 const pbNum =Math.floor(Math.random() * 26) + 1;
-
 pb.innerText = pbNum
 }
+
+btn.addEventListener("click", function (){
+randomNum()
 randomNumPb()
+})
 
