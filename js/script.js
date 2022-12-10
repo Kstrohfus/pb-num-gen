7333afb49679c3c8b1def07bdfa7e3d7b7a6b46a
Kstrohfus <houston11@me.com>
2022-12-09 21:23:27 -0700

    Function complete! project done unless want to style more

--- js/script.js
+++ js/script.js
@@ -1,26 +1,22 @@
 const pb = document.querySelector(".pb");
 const btn = document.querySelector(".btn");
+const btnOne = document.querySelector(".btn1");
 
 
 let elements = document.querySelectorAll(".num");
 for (let i = 0; i < elements.length; i++) {
-  elements[i].addEventListener("click", function() {
 function randomNum() {
 const num = Math.floor(Math.random() * 69) + 1;
 elements[i].innerText = num
-}randomNum()
-}) 
+}
+btnOne.addEventListener("click", function (){
+randomNum()})
 }
 
 function randomNumPb() {
 const numpb = Math.floor(Math.random() * 26) + 1;
 pb.innerText = numpb
 }
-
 btn.addEventListener("click", function (){
-
 randomNumPb()
 })
-
-
-
